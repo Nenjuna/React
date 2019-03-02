3caeb577d8c6ee545afa817c296d29ebb19e471f
Nenjuna <nenjunane@gmail.com>
2019-03-02 20:35:48 +0530

    Functions and variables

---
+++ src/components/Greet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h3>This is some function in Greet</h3>
+// }
+
+const Greet = () => <h3>This is an arrow function</h3>
+
+export default Greet
---
+++ src/components/Welcome.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class Welcome extends Component{
+    render(){
+        return(
+            <div className="Some">This is a welcome component</div>
+        
+        )
+    }
+}
+
+export default Welcome;
